2f774d243d7f25a9ab2076581a2231df12ae7938
taisyo0000 <itou.hiroki.kyoto@gmail.com>
2020-06-11 20:49:51 +0900

    Updates

---
+++ precache-manifest.c14b3de68755dd3ad2e5cef9101c822c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00ba2d9fb6c351afb4035035e8ac207",
+    "url": "/bioblog/index.html"
+  },
+  {
+    "revision": "36346ea36887c53932bf",
+    "url": "/bioblog/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "7acc5aac84960202aeae",
+    "url": "/bioblog/static/css/main.425ce245.chunk.css"
+  },
+  {
+    "revision": "36346ea36887c53932bf",
+    "url": "/bioblog/static/js/2.156315be.chunk.js"
+  },
+  {
+    "revision": "7acc5aac84960202aeae",
+    "url": "/bioblog/static/js/main.61ba672e.chunk.js"
+  },
+  {
+    "revision": "445f187def9782133acf",
+    "url": "/bioblog/static/js/runtime~main.3a3f0c6d.js"
+  },
+  {
+    "revision": "d7c0eb6ee290ab71af65ae3bd8347c69",
+    "url": "/bioblog/static/media/PixelMplus12-Bold.d7c0eb6e.ttf"
+  },
+  {
+    "revision": "9848197d6c91534133c521d787d275f3",
+    "url": "/bioblog/static/media/PixelMplus12-Regular.9848197d.ttf"
+  },
+  {
+    "revision": "a0179b5868fc89eef13b8d4efaf8588c",
+    "url": "/bioblog/static/media/bio-background.a0179b58.jpg"
+  }
+]);
